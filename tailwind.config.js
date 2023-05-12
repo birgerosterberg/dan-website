15c7f9ea9d490c955b8fde8fcf4077664e972263
Birger <birger.oesterber@gmail.com>
2023-05-12 13:47:18 +0200

    Restructure

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+    extend: {    
+      fontFamily: {
+        'robo': ['Roboto','sans-serif'],
+        'pop':['Poppins','sans-serif'],
+      },
+
+  
+  },
+
+  },
+
+  plugins: [],
+}
+
